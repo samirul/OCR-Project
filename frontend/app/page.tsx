cc7f679f9d3da8171e0c4665d76bceb8746aec45
Samirul Haque <samirulhaque08@gmail.com>
2026-03-09 18:51:36 +0530

    feat: add Radix UI components for accordion, checkbox, dropdown menu, input, label, navigation menu, sheet, and theme provider
    - Implemented accordion component with triggers and content.
    - Added checkbox component with custom styles.
    - Created dropdown menu with various items and sub-items.
    - Developed input and label components for form handling.
    - Integrated navigation menu with responsive design.
    - Built sheet component for modal-like functionality.
    - Added theme provider for managing light and dark themes.
    - Updated navbar to utilize new components and improve mobile experience.
    - Implemented theme toggle functionality using dropdown menu.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,12 @@
-export default function Home(){
-  return(
-    <div>Hello!</div>
-  )
+import { Navbar } from "@/components/web/navbar";
+import { NavbarNav } from "@/components/web/navbar-nav";
+
+export default function Home() {
+  
+  return (
+    <div>
+      <NavbarNav />
+      <h1>HIIIII</h1>
+    </div>
+  );
 }
